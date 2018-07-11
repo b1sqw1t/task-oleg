0ba8c88c3b030954fdbb0de2cbda5438c9f3898f
Oleg Mamaev <mamaev.oleg91@gmail.com>
2018-07-12 01:10:46 +0300

    last commit

--- cats/cats/settings.py
+++ cats/cats/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+import dj_database_url
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -25,7 +27,7 @@ SECRET_KEY = '!3&hy&wqoldt5206f#+(opi*8e@5(ito%w3%w&dccv-cg7drs^'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -123,3 +125,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/accounts/login/'
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
